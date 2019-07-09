3830dc22ff67e9768c77f76067337a9e3e68cb05
s1025162800 <1025162800@qq.com>
2019-07-09 22:43:42 +0800

    test

--- src/hellosunjob/Hellosunjob.java
+++ src/hellosunjob/Hellosunjob.java
@@ -4,7 +4,7 @@ public class Hellosunjob {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-   System.out.println("666");
+   System.out.println("666随便改一改");
 	}
 
 }
